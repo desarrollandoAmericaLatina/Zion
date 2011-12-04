976652ffad281ed963876a2758a000e4aef999ce
cristhianclx <cristhianclx@mail.com>
2011-12-03 21:06:35 -0500

    Se terminó el método para la inserción de data del DBF

--- 04_-_Construccion/zion/data/README.rst
+++ 04_-_Construccion/zion/data/README.rst
@@ -5,4 +5,6 @@
 ROBA.ME
 =======
 
-El archivo data.csv, al que se ha hace mención en el archivo fue generado desde el archivo (http://escale.minedu.gob.pe/descargas/mapa.aspx), del que se obtuvo las coordenadas geográficas de cada centro poblado, y luego guardarlo como csv.
+El archivo data.csv, al que se ha hace mención en el archivo import-dbf.py fue obtenido desde el archivo (http://escale.minedu.gob.pe/descargas/mapa.aspx), del que se obtuvo las coordenadas geográficas de cada centro poblado, y luego guardarlo se renombró de CP_P.dbf a data.dbf. Por razones obvias de espacio no se incluyó en el repositorio. Se tuvo que buscar que ningún campo de los importados posea el símbolo ' porque sino nos daba error. 
+
+Se hizo el método para que haga la inserción en la base de datos de SQLITE.
--- 04_-_Construccion/zion/data/import-dbf.py
+++ 04_-_Construccion/zion/data/import-dbf.py
@@ -1,11 +1,27 @@
 from dbfpy import dbf
-file = open('data.csv', 'r')  
-for line in f:  
-	line =  line.split(',')  
-#	product = Product() 
-#	product.name = line[2] + '(' + line[1] + ')'  
-#	product.description = line[4]  
-#	product.price = '' #data is missing from file  
-#	product.save()  
-	print line[0]
-f.close()
+import sqlite3
+from decimal import *
+
+file = './data.dbf'
+dbfFile = dbf.Dbf(file)
+
+connection = sqlite3.connect('../robame_bbdd')
+ado = connection.cursor()
+
+# Se va a usar los campos de modo que se ajusten al modelo definido en Django para la tabla Ciudad
+# - CODCP (5) --> codigoCiudad
+# - XGD (14) --> latitudCiudad
+# - YGD (15) --> longitudCiudad
+# - NOMCP (6) --> nombreCiudad
+# - DEP (2) --> departamento
+# - PROV (3) --> provincia
+# - DIST (4) --> distrito
+
+#for item in range(len(dbfFile)):
+for item in range(5):
+	sql = "INSERT INTO robame_ciudad(codigoCiudad, latitudCiudad, longitudCiudad, nombreCiudad, departamento, provincia, distrito) VALUES ('%s',%f,%f,'%s','%s','%s','%s')" % (dbfFile[item][4], dbfFile[item][13], dbfFile[item][14], dbfFile[item][5].replace("'", ""), dbfFile[item][1], dbfFile[item][2], dbfFile[item][3])
+	ado.execute(sql)
+connection.commit()
+connection.close()
+#	print Decimal(dbfFile[item][13])
+#	print dbfFile[item][14]
